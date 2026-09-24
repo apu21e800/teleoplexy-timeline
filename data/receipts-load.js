8fc01fb56ca8ed6c5a07eb432dd9873d2e915f63
Cleveland <pilot.vcs@gmail.com>
2026-09-24 15:30:31 -0700

    feat(shock-pass): receipts chunk loader + p6

---
+++ data/receipts-load.js
@@ -0,0 +1,4 @@
+window.__RP_SRC = (window.__RP0||'')+(window.__RP1||'')+(window.__RP2||'')+(window.__RP3||'')+(window.__RP4||'')+(window.__RP5||'')+(window.__RP6||'');
+eval(window.__RP_SRC);
+delete window.__RP0;delete window.__RP1;delete window.__RP2;delete window.__RP3;delete window.__RP4;delete window.__RP5;delete window.__RP6;
+delete window.__RP_SRC;
